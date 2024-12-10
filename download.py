1b73ae27e1a7fe6701fc13064632505e98d249ab
Peng Peng <billpengpeng@gmail.com>
2024-12-10 21:53:18 +0800

    add

--- download.py
+++ download.py
@@ -1,9 +1,8 @@
 
-import os
 from sf3d.system import SF3D
+from huggingface_hub import login
+login()
 
-
-print(os.environ.get('HUGGINGFACE_TOKEN')) 
 # Load the model
 model = SF3D.from_pretrained(
     "stabilityai/stable-fast-3d",
